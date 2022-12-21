00a35c60dd8db95c4a9dc39e024924d4b912f7b3
zen <949911292@qq.com>
2022-12-21 14:44:13 +0800

    get id

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <p>Your IP address is {{ ip }}</p>
+    <p>Your location is {{ location }}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      ip: '',
+      location: '',
+    };
+  },
+  created() {
+    axios.get('http://ip-api.com/json')
+      .then((response) => {
+        this.ip = response.data.query;
+        this.location = `${response.data.city}, ${response.data.country}`;
+      });
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
